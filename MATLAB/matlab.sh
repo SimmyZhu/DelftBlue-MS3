3a332f04b521062ef199a3ab9e847f41318f8f0b
Simmy <70203356+SimmyZhu@users.noreply.github.com>
2022-08-28 14:47:56 +0200

    Update matlab.sh

--- MATLAB/matlab.sh
+++ MATLAB/matlab.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 #
-#SBATCH --job-name="matlab_preprocessing"
+#SBATCH --job-name="matlab_code"
 #SBATCH --partition=compute
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
+#SBATCH --account=research-eemcs-me
 
 module load 2022r1
 module load matlab
 
-srun matlab -batch "run('/home/szhu2/CS4245_Project/MATLAB/main.m'); exit;"
+srun matlab -batch "run('~/DelftBlue-MS3/MATLAB/examplecode.m'); exit;"
