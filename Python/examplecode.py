1c920bc826910c1aecaae3820028f5fd6e12b75f
Simmy <70203356+SimmyZhu@users.noreply.github.com>
2022-08-28 15:08:43 +0200

    Update examplecode.py

--- Python/examplecode.py
+++ Python/examplecode.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 # Some example data to display
 x = np.linspace(0, 2 * np.pi, 400)
 y = np.sin(x ** 2)
 
 # Plot and Save
+dir_cwd = os.getcwd()
 plt.plot(x, y)
-plt.savefig('~/DelftBlue-MS3/Python/example.png')
+plt.savefig(os.path.join(dir_cwd, 'example.png'))
 print('Job finished')
