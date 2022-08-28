090aa92a8bab63a9f1e41d3a7c223e2d70f682d9
Simmy <70203356+SimmyZhu@users.noreply.github.com>
2022-08-28 13:26:45 +0200

    Update sub_data_transfer.sh

--- MATLAB/sub_data_transfer.sh
+++ MATLAB/sub_data_transfer.sh
@@ -7,5 +7,5 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --account=research-eemcs-me
 
-rsync -av /tudelft.net/staff-groups/ewi/me/MS3/MS3-Shared/RadarScenes/RadarScenes/RadarScenes/data/sequence_101/camera /home/szhu2/DelftBlue-MS3/MATLAB
+rsync -av /tudelft.net/staff-groups/ewi/me/MS3/MS3-Shared/RadarScenes/RadarScenes/RadarScenes/data/sequence_101/camera/22549784615285.jpg /home/szhu2/DelftBlue-MS3/MATLAB
 
