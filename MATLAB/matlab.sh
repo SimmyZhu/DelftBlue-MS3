946d2e76af33064106b058381276e03db6c20587
Simmy <70203356+SimmyZhu@users.noreply.github.com>
2023-05-23 13:28:34 +0200

    Update matlab.sh
    To educational account

--- MATLAB/matlab.sh
+++ MATLAB/matlab.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
-#SBATCH --account=research-eemcs-me
+#SBATCH --account=education-eemcs-courses-ee4675
 
 module load 2022r1
 module load matlab
